7c0aa5b3a4b5e0ad499562e3a84d298aefeb85dc
muhammadaysh <mddaysh@gmail.com>
2024-05-31 14:55:30 +0900

    Remove unused TelloStreamModule import

---
+++ android/app/src/main/java/com/anonymous/Kizuna/TelloStreamPakcage.java
@@ -0,0 +1,22 @@
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.ReactApplicationContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TelloStreamPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new TelloStreamModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> managers = new ArrayList<>();
+        managers.add(new StreamViewManager());
+        return managers;
+    }
+}
